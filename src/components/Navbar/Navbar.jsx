4d13fcff6a7164bbb7cf8b31c0b0ae432c8ba08d
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2026-03-28 22:01:57 -0300

    Se arreglo problema con brarra de navegacion principalmente en el boton de contacto

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 import Logo from '../Logotipo/Logo.jsx'
 import ButtonDropdown from './ButtonDropdown/ButtonDropdown'
 import './Navbar.scss'
@@ -20,9 +21,15 @@ export default function Navbar() {
                     <NavLink to="/" className='link-BarraDeNavegacion'> HOME </NavLink>
                     <NavLink to="/Portfolio" className='link-BarraDeNavegacion'>PORTFOLIO</NavLink>
                     <NavLink to="/Acerca-de-mi" className='link-BarraDeNavegacion'>ACERCA DE MI</NavLink>
-                    <a className='boton-contacto-BarraDeNavegacion' href='#formas-de-contacto'><div>CONTACTO</div></a>
+                    <HashLink 
+                      smooth 
+                      to="/#formas-de-contacto" 
+                      className='boton-contacto-BarraDeNavegacion'
+                    >
+                      <div>CONTACTO</div>
+                    </HashLink>                    
                 </div>
-                <div className='contenedor-boton-desplegable-BarraDeNavegacion' onClick={() => { setMenuDespegado(!menuDesplegado) }}>
+                <div className='contenedor-boton-desplegable-BarraDeNavegacion' onClick={() => { z(!menuDesplegado) }}>
                     <ButtonDropdown estadoDesplegado={menuDesplegado} />
                 </div>
             </nav>
