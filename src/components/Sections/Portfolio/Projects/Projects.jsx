4cdd638672b4ebcd50e66ded0b5f9360bfd2ec69
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2026-03-30 16:44:50 -0300

    Se arreglo estilos en la pagina Portfolio, en la seccion Habilidades, Experiencias y Proyectos

--- src/components/Sections/Portfolio/Projects/Projects.jsx
+++ src/components/Sections/Portfolio/Projects/Projects.jsx
@@ -9,10 +9,14 @@ export default function Projects() {
             <header className='header-Projects'>
                 <h2> PROYECTOS PORTFOLIO</h2>
             </header>
+            <section className='section-Projects'>
+              <div className='text-Projects '>
+                <p >
+                    Mi portfolio cuenta con proyectos que son, en general, pruebas técnicas realizadas para diferentes empresas, y que estas utilizan para la selección de personal. Cada proyecto se enfocó en desarrollar soluciones web a las diferentes problemáticas planteadas y detallada en los requerimientos de cada Challenge, utilizando las tecnologías solicitadas, generalmente ReactJS como librería principal para la construcción de interfaces de usuario interactivas y amigables. Los proyectos en conjunto demuestran mi capacidad para desarrollar soluciones web de alta calidad y escalables en React, y cumplir con los plazos establecidos.
+                </p>
+              </div>
+            </section>
             
-            <p className='text-Projects '>
-                Mi portfolio cuenta con proyectos que son, en general, pruebas técnicas realizadas para diferentes empresas, y que estas utilizan para la selección de personal. Cada proyecto se enfocó en desarrollar soluciones web a las diferentes problemáticas planteadas y detallada en los requerimientos de cada Challenge, utilizando las tecnologías solicitadas, generalmente ReactJS como librería principal para la construcción de interfaces de usuario interactivas y amigables. Los proyectos en conjunto demuestran mi capacidad para desarrollar soluciones web de alta calidad y escalables en React, y cumplir con los plazos establecidos.
-            </p>
             <ProjectsList proyectos={proyectos} />
         </div>
     )
--- src/components/Sections/Portfolio/Skills/Skills.jsx
+++ src/components/Sections/Portfolio/Skills/Skills.jsx
@@ -6,31 +6,40 @@ import ListSkills from './ListSkills/ListSkills'
 import SkillsDescription from './SkillsDescription/SkillsDescription'
 
 export default function Skills() {
-    const ActividadesRealizadas1 = responseSkills.ActividadesDEV
-    const ActividadesRealizadas2 = responseSkills.ActividadesUIX
-    const SkillsDev = responseSkills.SkillsDev
-    const SkillsDesing = responseSkills.SkillsUIX
+  const ActividadesRealizadas1 = responseSkills.ActividadesDEV
+  const ActividadesRealizadas2 = responseSkills.ActividadesUIX
+  const SkillsDev = responseSkills.SkillsDev
+  const SkillsDesing = responseSkills.SkillsUIX
 
-    return (
-        <div className='container-Skills'>
-            <div className='pre-header-Skills' />
-            <ImageComparator />
-            <div className='post-header-Skills' />
+  return (
+    <div className='container-Skills'>
+      <div className='pre-header-Skills' />
+      <ImageComparator />
+      <div className='post-header-Skills' />
 
-            <header>
-                <h2>HABILIDADES</h2>
-            </header>
-            <main>
-                <div className='container-text-presentacion-Skills'>
-                    <p>Actualmente soy Developer Web FrontEnd con diversas habilidades en lo que respecta al desarrollo de software, y al mundo IT que lo rodea.</p>
-                    <p>Incursionando en el mundo BackEnd, para alcanzar la meta de ser FullStack, y mejorando día a día, mediante la utilización de patrones de diseño y buenas prácticas para el desarrollo de software.</p>
-                    <p>Además cuento con diferentes habilidades en el área del Diseño de Experiencias e Interfaces de Usuario, con las cual se le agrega valor al desarrollo web, mejorando así  la experiencia que tienen las personas al utilizar las aplicaciones Web, y facilitando la comprensión e interactividad con la aplicación, e impactar directamente en la rentabilidad de tu negocio.</p>
-                </div>
-                
-                <SkillsDescription Actividades1={ActividadesRealizadas1} Actividades2={ActividadesRealizadas2} />
+      <header>
+        <h2>HABILIDADES</h2>
+      </header>
+      <main>
+        <section className='section-description-skills'>
+          <div className='container-text-presentacion-Skills'>
+            <div>
+              <p>Actualmente soy Developer Web FrontEnd con diversas habilidades en lo que respecta al desarrollo de software, y al mundo IT que lo rodea.</p>
+              <p>Incursionando en el mundo BackEnd, para alcanzar la meta de ser FullStack, y mejorando día a día, mediante la utilización de patrones de diseño y buenas prácticas para el desarrollo de software.</p>
+              <p>Además cuento con diferentes habilidades en el área del Diseño de Experiencias e Interfaces de Usuario, con las cual se le agrega valor al desarrollo web, mejorando así  la experiencia que tienen las personas al utilizar las aplicaciones Web, y facilitando la comprensión e interactividad con la aplicación, e impactar directamente en la rentabilidad de tu negocio.</p>
+            </div>
+           
+          </div>
+          <SkillsDescription Actividades1={ActividadesRealizadas1} Actividades2={ActividadesRealizadas2} />
 
-                <ListSkills SkillsDev={SkillsDev} SkillsDesing={SkillsDesing} />
-            </main>
-        </div>
-    )
+        </section>
+        <section className='section-list-skills'>
+
+          <ListSkills SkillsDev={SkillsDev} SkillsDesing={SkillsDesing} />
+        </section>
+
+
+      </main>
+    </div>
+  )
 }
